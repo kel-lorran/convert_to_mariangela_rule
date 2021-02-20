276e12316fb0eeb7e2a2949109bb8a8558963e47
kelvin jesus <kelvinlorran2017@gmail.com>
2021-02-20 02:03:03 -0300

    versao inicial, o output aparentemente está correto, mas não foi testado em um banco ainda

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Doc from './model/Doc';
+import Line from './model/Line';
 import './App.css';
 
 function App() {
+  const [contentToConvert, setContentToConvert] = useState('');
+  const [contentConverted, setContentConverted] = useState('');
+
+  const handleChange = ({ target: {value}}) => setContentToConvert(value);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const doc = new Doc(contentToConvert)
+    setContentConverted(doc.convert())
+  } 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Convertar seus arquivos .ddl gerados automaticamente pelo Oracle Data Modeler para o padão Mariangela</h1>
+      <form onSubmit={handleSubmit} onChange={handleChange}>
+        <textarea placeholder="cole seu codigo aqui" required>
+        </textarea>
+        <button type="submit">Converter</button>
+        {/* <textarea> */}
+          <pre>
+            {contentConverted}
+          </pre>
+        {/* </textarea> */}
+      </form>
     </div>
   );
 }
